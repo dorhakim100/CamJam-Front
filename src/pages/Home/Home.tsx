e17e8b579175e58d16deeede61fc461f8b2f6d66
Dor <dorhakim100@gmail.com>
2025-05-24 17:13:05 +0300

    Add two home buttons

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,9 @@ import { useSelector } from 'react-redux'
 import { RootState } from '../../store/store'
 import { ClockTime } from '../../components/ClockTime/ClockTime'
 
+import { BsFillCameraVideoFill } from 'react-icons/bs'
+import { BsPlusSquareFill } from 'react-icons/bs'
+
 export function Home() {
   const prefs = useSelector(
     (stateSelector: RootState) => stateSelector.systemModule.prefs
@@ -14,11 +17,15 @@ export function Home() {
       label: 'New meeting',
       action: () => console.log('Button 1 clicked'),
       id: 'new-meeting-button',
+      icon: <BsFillCameraVideoFill className='icon' />,
+      color: '#F26D21',
     },
     {
       label: 'Join',
       action: () => console.log('Button 2 clicked'),
       id: 'join-button',
+      icon: <BsPlusSquareFill className='icon' />,
+      color: '#2D8CFF',
     },
   ]
 
@@ -27,13 +34,21 @@ export function Home() {
       <ClockTime />
       <div className='buttons-container'>
         {buttons.map((button) => (
-          <button
-            key={button.id}
-            className='home-button'
-            onClick={button.action}
+          <div
+            className={`button-container ${
+              prefs.isDarkMode ? 'dark-mode' : ''
+            }`}
           >
-            {button.label}
-          </button>
+            <button
+              key={button.id}
+              className='home-button'
+              onClick={button.action}
+              style={{ backgroundColor: button.color }}
+            >
+              {button.icon}
+            </button>
+            <span>{button.label}</span>
+          </div>
         ))}
       </div>
     </div>
