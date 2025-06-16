44373b15a8348f7bb52310699d1c837e77d03d76
Dor <dorhakim100@gmail.com>
2025-06-16 19:28:30 +0300

    Add initial message-container styling

--- src/components/MessagePreview/MessagePreview.tsx
+++ src/components/MessagePreview/MessagePreview.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/store'
 
 export function MessagePreview({ message }: { message: any }) {
+  const prefs = useSelector(
+    (stateSelector: RootState) => stateSelector.systemModule.prefs
+  )
+
   return (
     <li
       key={message.id}
-      className={`message-container ${message.isMe ? 'me' : ''}`}
+      className={`message-container ${message.isMe ? 'me' : ''} ${
+        prefs.isDarkMode ? 'dark-mode' : ''
+      }`}
     >
+      <span className='user-name'>
+        {message.isMe ? 'You' : message.userName || 'Unknown User'}
+      </span>
       <img className='user-image' src={message.userImg} alt='user' />
       <span className='message-text'>{message.text}</span>
     </li>
